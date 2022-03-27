1751e4ed38d4b57bd6d592d67d9e308404cc5a92
Michel França <fr.mich17@gmail.com>
2022-03-27 08:30:19 -0300

    Correção na configuração de conexão do mongodb.

--- src/configs/connectionMongoDB.js
+++ src/configs/connectionMongoDB.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const databaseConnection = process.env.DATABASE_CONNECTION
+const databaseConnection = process.env.DATABASE_CONNECTION || 'mongodb://localhost/meet'
 
 mongoose.connect(databaseConnection)
 mongoose.Promise = global.Promise
