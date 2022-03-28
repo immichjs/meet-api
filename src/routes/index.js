e5c374b0d8c57f5c47e07ab1fa8efeb7c8ef3adf
Michel França <fr.mich17@gmail.com>
2022-03-28 00:28:08 -0300

    Documentação das rotas disponíveis

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,76 @@ const routes = Router()
 
 routes.use(accountRoutes)
 routes.use(userRoutes)
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }))
+routes.get('/', (req, res) => res.json([
+  {
+    resource: '/register',
+    method: 'POST',
+    mandatoryAttributes: {
+      name: 'String',
+      email: 'String',
+      cpf: 'String',
+      phone: 'String',
+      password: 'String'
+    },
+    return: {
+      user: {
+        name: '',
+        email: '',
+        cpf: '',
+        phone: '',
+        status: true,
+        userType: '',
+        createdAt: '',
+        updatedAt: '',
+        _id: ''
+      },
+      token: ''
+    }
+  }, {
+    resource: '/login',
+    method: 'POST',
+    mandatoryAttributes: {
+      email: 'String',
+      cpf: 'String',
+      password: 'String'
+    },
+    return: {
+      user: {
+        name: '',
+        email: '',
+        cpf: '',
+        phone: '',
+        status: true,
+        userType: '',
+        createdAt: '',
+        updatedAt: '',
+        _id: ''
+      },
+      token: ''
+    }
+  }, {
+    resource: '/forgot_password',
+    method: 'POST',
+    mandatoryAttributes: {
+      email: 'String',
+      cpf: 'String',
+    },
+    return: {
+      user: {
+        email: ''
+      },
+      message: ''
+    }
+  }, {
+    resource: '/reset_password',
+    method: 'POST',
+    mandatoryAttributes: {
+      email: 'String',
+      token: 'String',
+      password: 'String'
+    },
+  }
+]
+))
 
 module.exports = routes
